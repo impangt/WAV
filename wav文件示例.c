fc76549cb1bc5ee561443d00e06ed33dfb81e9c8
SCUTPangty <1179478742@qq.com>
2026-04-15 22:57:09 +0800

    Add files via upload

---
+++ wav文件示例.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <math.h>
+
+typedef int16_t i16;
+typedef uint16_t u16;
+typedef uint32_t u32;
+
+typedef float f32;
+
+void write_le_16(FILE *f, u16 n){
+    fwrite(&n, sizeof(u16), 1, f);
+}
+
+void write_le_32(FILE *f, u32 n){
+    fwrite(&n, sizeof(u32), 1, f);
+}
+
+#define WRITE_STR_LIT(f, s) fwrite((s), 1, sizeof(s)-1,f)
+
+#define FREQ 44100
+#define DURATION 3
+
+int main(void)
+{
+    FILE* f = fopen("test.wav", "wb");
+
+    u32 num_sample = DURATION * FREQ;
+    u32 file_size = num_sample * sizeof(u16) + 44;
+
+    WRITE_STR_LIT(f, "RIFF");//RIFF 的ID
+    write_le_32(f,file_size - 8);//文件总大小
+    WRITE_STR_LIT(f,"WAVE");//说明装了音频wave文件
+    WRITE_STR_LIT(f, "fmt ");//参数区证明
+    write_le_32(f,16);//fmt区块本身的大小
+    write_le_16(f,1);//编码格式PCM
+    write_le_16(f,1);//通道数
+    write_le_32(f, FREQ);//采样率（一般是44100Hz
+    write_le_32(f, FREQ * sizeof(u16));//采样率*声道*位深/8
+    write_le_16(f, sizeof(u16));//声道*位深/8
+    write_le_16(f, sizeof(u16) * 8);//每一位采样的比特率（16位）
+
+    WRITE_STR_LIT(f, "data");//往后全是原始采样点
+    //标记后面音频数据总长度（字节数）
+    write_le_32(f, num_sample * sizeof(u16));
+
+    for(u32 i=0; i< num_sample ; i++)
+    {//信号发生器
+        f32 t = (f32)i / FREQ;
+        //信号波动，440是频率，后面是圆周率
+        f32 y = 0.25f * sinf(t * 440.0f * 2.0f * 3.1415926535f);
+        i16 sample = (i16)(y * INT16_MAX);
+
+        write_le_16(f, sample);
+    }
+
+    fclose(f);
+
+    return 0;
+}
